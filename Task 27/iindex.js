6c48298c5e40479f8ed095657d0a636d7a1198e0
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:52:30 -0800

    Update iindex.js

--- Task 27/iindex.js
+++ Task 27/iindex.js
@@ -1,1 +1,18 @@
-console.log("hello World")
+//Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+//• If the alien is green, print a message that the player earned 5 points.
+//• If the alien is yellow, print a message that the player earned 10 points.
+//• If the alien is red, print a message that the player earned 15 points.
+//• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+const alien_colour = function (Colour) {
+  if (Colour == "Green") {
+    console.log("Player just earned 5 points");
+  } else if (Colour == "Yellow") {
+    console.log("Player just earned 10 points");
+  } else if (Colour == "Red") {
+    console.log("Player just earned 15 points");
+  } else {
+  }
+};
+
+alien_colour("Yellow");
