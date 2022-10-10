4d4b1948a9ed603971326bc4c39face861fc6925
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:53:08 -0800

    Update index.js

--- Task 28/index.js
+++ Task 28/index.js
@@ -1,1 +1,19 @@
-console.log("hello World")
+//Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+
+const Life_stages = function (age) {
+  if (age < 2) {
+    console.log("Baby");
+  } else if ((age >= 2, age < 4)) {
+    console.log("Toddler");
+  } else if ((age >= 4, age < 13)) {
+    console.log("Kid");
+  } else if ((age >= 13, age < 20)) {
+    console.log("Teenage");
+  } else if ((age >= 20, age < 65)) {
+    console.log("Adult");
+  } else {
+    console.log("Elder");
+  }
+};
+
+Life_stages(19);
