d064bdbd90344af5de54486bb5f1c86423af36fb
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:50:05 -0800

    Update index.js

--- Task 23/index.js
+++ Task 23/index.js
@@ -1,1 +1,29 @@
-console.log("hello World")
+//Ternanry Operator
+
+//Test#1
+let car = "subaru";
+
+console.log("Is car == 'BMW'? I predict False.");
+console.log(car == "BMW");
+
+//Test#2
+let a = 5;
+let b = 6;
+
+console.log("Is a > b ? I predict False.");
+console.log(a > b);
+
+//Test#3
+console.log("Is a is equal b ? I predict False.");
+console.log(a == b);
+
+//Test#4
+let result = a > b ? "a is greater than b" : "b is greater than a";
+console.log("Is a > b or b > a? I predict b is greater than a");
+console.log(result);
+
+//Test#5
+let city = "KARCHI";
+
+console.log("Where do you live? I predict Lahore");
+console.log(city == "lahore");
