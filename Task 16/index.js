a5c6dbceedf0406dcba2081e150e802e4d601555
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:45:06 -0800

    Update index.js

--- Task 16/index.js
+++ Task 16/index.js
@@ -1,1 +1,16 @@
-console.log("hello World")
+//More Guests: You just found a bigger dinner table, so now more
+//space is available. Think of three more guests to invite to dinner.
+
+var Guest_list = [
+  '"SYED Hamad AZIZ"',
+  '"ali khan"',
+];
+
+Guest_list.unshift('"Hazrat Abu Bakr Siddique R.A"'); //adding at the beginning of array
+Guest_list.push('"ALI AHMED"');
+console.log("I have got a bigger space and I now love to invite more");
+
+for (var i = 0; i < Guest_list.length; i++) {
+  var message = `DEAR, ${Guest_list[i]}, I would like to invite to dinner.`;
+  console.log(message);
+}
