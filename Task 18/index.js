82e9d3df9474e6b429705a2576a8876a26dabdb1
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:46:33 -0800

    Update index.js

--- Task 18/index.js
+++ Task 18/index.js
@@ -1,1 +1,16 @@
-console.log("hello World")
+//Seeing the World: Think of at least five places in the world you’d like to visit.
+
+const places = ["India", "Dubai", "Medinah", "Makkah", "Swizerland"];
+console.log(places); // Without order
+console.log(places.concat().sort()); //In order without changig original array
+console.log(places); //Checking the order
+console.log(places.slice().reverse()); //In reverse alphabetical order without changing the order of the original list
+console.log(places); //Checking the order
+console.log(places.reverse());// Reverse the order.
+console.log(places); //Checking the order
+console.log(places.reverse());// Reverse the order.
+console.log(places); //Checking the order
+console.log(places.sort());//Sort your array so it’s stored in alphabetical order.
+console.log(places); //Checking the order
+console.log(places.sort().reverse()); //Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+console.log(places); //Checking the order
