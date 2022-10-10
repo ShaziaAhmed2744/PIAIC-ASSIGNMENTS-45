9a7bce7f2b4d8296f24333be79c79b33dc989335
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:47:20 -0800

    Update index.js

--- Task 19/index.js
+++ Task 19/index.js
@@ -1,1 +1,10 @@
-console.log("hello World")
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+//print a message indicating the number of people you are inviting to dinner.
+
+var list = ['"ali raza AZIZ"', '"SYED KOKAB HASSAN"', '"HAZRAT ALI R.A"'];
+for (var i = 0; i < list.length; i++) {
+  var message = `DEAR, ${list[i]}, I would like to invite to dinner.`;
+  console.log(message);
+}
+console.log(`I am inviting ${list.length} people to the dinner.`);
+//console.log(list[5]);
