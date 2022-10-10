d3e5ff76cc5f6b3edb64a0cfe6c9fa1328979528
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:58:21 -0800

    Update index.js

--- Task 36/index.js
+++ Task 36/index.js
@@ -1,1 +1,8 @@
-console.log("hello World")
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+const make_shirt = function (size, message) {
+  console.log(
+    `Your shirt size is ${size} and the message that print on shirt is ${message}`
+  );
+};
+make_shirt(32, '"GOOD BYE"');
