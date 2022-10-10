e76bdf63f1b0d3b171e529da1d9a131618bb7375
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:57:03 -0800

    Update index.js

--- Task 34/index.js
+++ Task 34/index.js
@@ -1,1 +1,15 @@
-console.log("hello World")
+//Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+//• Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+//• Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+
+const fav_pizza = ["BBQ Pizza", "Pepproni Pizza", "Fagita Pizza"];
+
+fav_pizza.forEach(function (pizza) {
+  console.log(pizza);
+});
+
+fav_pizza.forEach(function (pizza_sen) {
+  console.log(`I like ${pizza_sen} very much!`);
+});
+
+console.log("I really like pizza!");
