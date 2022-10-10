2d2bece39781f2f827d48b536d9cf84ecb5ad66c
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:45:52 -0800

    Update index.js

--- Task 17/index.js
+++ Task 17/index.js
@@ -1,2 +1,17 @@
 
-console.log("hello World")
+//Shrinking Guest List: You just found out that your new dinner
+//table won’t arrive in time for the dinner, and you have space for only two guests.var Guest_list = [
+
+var Guest_list = [
+  '"SYED HATIM AZIZ"',
+  '"Hazrat Abu Bakr Siddique R.A"',
+  '"HAZRAT ALI R.A"',
+  '"ALI AHMED"',
+];
+
+for (var i = Guest_list.length - 1; i >= 2; i--) {
+  var message = `Dear, ${Guest_list[i]} I can invite only two people for dinner. Sorry can't invite you to dinner.`;
+  console.log(message);
+  Guest_list.pop(i);
+}
+console.log(Guest_list);
