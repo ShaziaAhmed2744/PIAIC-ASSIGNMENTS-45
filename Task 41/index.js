4869a4181b14cdd41e7d7f0978aaed01d9701211
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 02:01:23 -0800

    Update index.js

--- Task 41/index.js
+++ Task 41/index.js
@@ -1,1 +1,11 @@
-console.log("hello World")
+
+//Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+
+const magician_name = ["flardo", "Shakrt"];
+
+const show_magicians = function (magician_name) {
+  console.log(magician_name);
+};
+magician_name.forEach(function (magician_name) {
+  show_magicians(magician_name);
+});
