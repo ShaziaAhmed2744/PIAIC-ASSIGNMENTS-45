7e736eaa0f04299a3a6122354b3db944c47b1f67
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 02:00:13 -0800

    Update index.js

--- Task 39/index.js
+++ Task 39/index.js
@@ -1,1 +1,8 @@
-console.log("hello World")
+//City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+
+const city_country = function (city, country) {
+  console.log(`${city}, ${country}`);
+};
+city_country("Karachi", "Pakistan");
+city_country("Istanbol", "Turkey");
+city_country("Delhi", "India");
