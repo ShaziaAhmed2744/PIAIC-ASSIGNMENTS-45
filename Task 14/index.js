2a8ed80e3d6239d87355e1d887ef6d124f88105e
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:43:31 -0800

    Update index.js

--- Task 14/index.js
+++ Task 14/index.js
@@ -1,1 +1,10 @@
-console.log("hello World")
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would
+//you invite? Make a list that includes at least three people you’d like to invite
+//to dinner. Then use your list to print a message to each person,
+//inviting them to dinner.
+
+var list = ['"sara ali"', '"SYED KOKAB HASSAN"', '"HAZRAT ALI R.A"'];
+for (var i = 0; i < list.length; i++) {
+  var message = `DEAR, ${list[i]}, I would like to invite to dinner.`;
+  console.log(message);
+}
