d61cb6c972292e60f72eb06ada1959b83a9ba47b
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 02:04:28 -0800

    Update index.js

--- Task 45/index.js
+++ Task 45/index.js
@@ -1,1 +1,10 @@
-console.log("hello World")
+//Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+function Cars(name, model, colour) {
+  let Car = {
+    Name: name,
+    Model: model,
+    Colour: colour,
+  };
+  console.log(Car.Name, Car.Model, Car.Colour);
+}
+Cars("Civic", "2000", "White");
