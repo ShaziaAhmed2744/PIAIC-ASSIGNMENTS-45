9001424e9f9d3e0b4afe1ba590df57e39fe8c623
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:51:07 -0800

    Update index.js

--- Task 25/index.js
+++ Task 25/index.js
@@ -1,1 +1,27 @@
-console.log("hello World")
+//Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+//• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+//• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+const alien_color = function (Colour) {
+  if (Colour == "Green") {
+    console.log("Player just earned 5 points");
+  } else {
+  }
+};
+
+//Test
+const test = function (Colour) {
+  if (Colour == "Green") {
+    console.log("Test pass");
+  } else {
+    console.log("Test not pass");
+  }
+};
+
+alien_color("Green");
+
+//TEST PASS
+test("Green");
+
+//TEST NOT PASS
+test("Red");
