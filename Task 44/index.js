90ccc32422af1a4a27c28fe4cf5ac22b91b944c8
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 02:03:35 -0800

    Update index.js

--- Task 44/index.js
+++ Task 44/index.js
@@ -1,1 +1,7 @@
-console.log("hello World")
+//Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+
+const sandwich = function (Sauce, Bread, Colddrink, Fries) {
+  console.log(`${Sauce}, ${Bread}, ${Colddrink}, ${Fries}`);
+};
+
+sandwich("Mayo", "Soft", "Diet", "Yes");
