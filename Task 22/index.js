d0def3dd415fc697bd05d5a1b3f299c0cfa84da4
Shazia Ahmed <shaziaahmed2744@gmail.com>
2022-10-10 01:49:26 -0800

    Update index.js

--- Task 22/index.js
+++ Task 22/index.js
@@ -1,1 +1,12 @@
-console.log("hello World")
+//Intentional Error: If you haven’t received an array index error
+//in one of your programs yet, try to make one happen. Change an index in
+//one of your programs to produce an index error.
+//Make sure you correct the error before closing the program.
+
+var list = ['"SYED HATIM AZIZ"', '"SYED KOKAB HASSAN"', '"HAZRAT ALI R.A"'];
+for (var i = 0; i < list.length; i++) {
+  var message = `DEAR, ${list[i]}, I would like to invite to dinner.`;
+  console.log(message);
+}
+console.log(`I am inviting ${list.length} people to the dinner.`);
+//console.log(list[5]); Intentional Error:
